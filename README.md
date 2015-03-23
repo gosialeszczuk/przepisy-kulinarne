0c1b8f60a8e610cb36377a616b00f4bcc1898f6a
kamilstrzelecki <kstrzelecki@outlook.com>
2015-03-23 21:43:43 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Przysłowia
+==========
+
+[![Build Status](https://travis-ci.org/by-examples/zad-przyslowia.svg)](https://travis-ci.org/by-examples/zad-przyslowia)
+
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('default/index.html.twig');
+    }
+    /*
+     * G1
+     */
+    /*
+     * G2
+     */
+    /*
+     * P1
+     */
+    /*
+     * P2
+     */
+    /*
+     * R1
+     */
+    /*
+     * R2
+     */
+    /*
+     * A1
+     */
+    /*
+     * A2
+     */
+   
+
+}
